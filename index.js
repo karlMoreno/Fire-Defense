7dcd645b68b34329e824115ac5d9cee5eb2eda93
karlMoreno <karlcmoreno0811@gmail.com>
2022-12-20 14:21:20 -0800

    Create a Player

--- index.js
+++ index.js
@@ -5,3 +5,29 @@ canvas.height = innerHeight //same thing
 const c = canvas.getContext('2d') //context for canvas api to do graphics for a 2d game
 
 console.log(c)
+
+
+class Player {
+    constructor(x,y,radius,color) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+    } // end of constructor
+
+    draw() {
+        c.beginPath()
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        c.fillStyle = this.color
+        c.fill()
+
+    }// end of draw
+} // end of player
+
+const x = canvas.width / 2
+const y = canvas.height / 2
+
+
+const player = new Player(x,y,30,'blue')
+player.draw()
+console.log(player)
