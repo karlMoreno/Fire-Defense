c2254a74c698fb66f5530ef92bdbcbbc3ca0f17b
karlMoreno <karlcmoreno0811@gmail.com>
2023-01-09 13:33:31 -0800

    Setting up localhost

--- FD-database.js
+++ FD-database.js
@@ -2,3 +2,10 @@ const express = require('express')
 const app = express()
 const port = 8080
 const mysql = require('mysql')
+
+app.get('/', (req,res) => {
+    res.send('New York, New York')
+})
+app.listen(port, () => {
+    console.log(`'I call thee from ${port}'`)
+})
