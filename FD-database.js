1e547b73063521628f1281cbc64fb538caa7c737
karlMoreno <karlcmoreno0811@gmail.com>
2023-02-07 22:51:21 -0800

    Setting up Top Score Table

--- FD-database.js
+++ FD-database.js
@@ -1,21 +1,24 @@
 const express = require('express')
-const { read } = require('fs')
 const app = express()
 const port = 8080
 const mysql = require('mysql')
 
+const conn = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'password',
+    database: 'MYSQL_DATABASE'
+})
+
+conn.connect()
 
 function readDB (callback) {
-    const conn = mysql.createConnection({
-        host: '127.0.0.1',
-        user: 'root',
-        password: 'students',
-        database: 'MYSQL_DATABASE'
-    })
-
     conn.connect(function err(){
-        if (err) throw err
-        conn.query("SELECT * FROM FireDefenseDB", function(err,result,fields){
+        if (err) {
+            console.log(err.message);
+            throw err;
+        }
+        conn.query("SELECT * FROM MYSQL_DATABASE.FireDefenseDB", function(err,result,fields){
             if (err) throw err
             callback(result)
         })
@@ -27,13 +30,18 @@ function readDB (callback) {
 app.use('/', express.static('public'))
 
 app.get('/init', (req,res) => {
-    readDB(function callback(var1){
-        res.end(JSON.stringify(var1))
+    conn.query("SELECT * FROM MYSQL_DATABASE.FireDefenseDB", function(err,result,fields){
+        if (err) throw err
+        // callback(result)
+        res.send({success : true, data: result})
     })
+// readDB(function callback(var1){
+    //     res.end(JSON.stringify(var1))
+    // })
 })
 
 app.get('/test', (req,res) => {
-    res.send
+    res.send()
 })
 
 app.get('/', (req,res) => {
